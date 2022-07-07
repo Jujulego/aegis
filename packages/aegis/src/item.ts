652435c8bfe535bf97ca10e778df78609a7e6b5a
Julien Capellari <julien.capellari@gmail.com>
2022-07-07 16:52:00 +0200

    feat(aegis): pass item events

--- packages/aegis/src/item.ts
+++ packages/aegis/src/item.ts
@@ -1,24 +1,41 @@
-import { Entity, Item, Query, RefreshStrategy } from '@jujulego/aegis-core';
+import {
+  Entity,
+  EventListener,
+  EventListenerOptions, EventSource, EventType,
+  EventUnsubscribe, ExtractKey, Item,
+  PartialKey, Query, QueryManagerEventMap, RefreshStrategy,
+  StoreEventMap
+} from '@jujulego/aegis-core';
 
 import { AegisId, Refreshable } from './utils';
 
 // Types
-export interface AegisItem<T, I extends AegisId = AegisId> {
-  readonly $id: I;
-  readonly $item: Item<T>;
+interface AegisItemBase<T> {
   readonly $entity: Entity<T>;
+
+  subscribe(
+    key: 'update',
+    listener: EventListener<StoreEventMap<T>, `update.${string}.${string}`>,
+    opts?: EventListenerOptions
+  ): EventUnsubscribe;
+  subscribe<T extends PartialKey<EventType<QueryManagerEventMap<T>>>>(
+    type: T,
+    listener: EventListener<QueryManagerEventMap<T>, ExtractKey<EventType<QueryManagerEventMap<T>>, T>>,
+    opts?: EventListenerOptions
+  ): EventUnsubscribe;
 
   readonly isLoading: boolean;
   readonly data?: T;
 }
 
-export interface AegisUnknownItem<T, I extends AegisId = AegisId> {
+export interface AegisItem<T, I extends AegisId = AegisId> extends AegisItemBase<T> {
+  readonly $id: I;
+  readonly $item: Item<T>;
+}
+
+export interface AegisUnknownItem<T, I extends AegisId = AegisId> extends AegisItemBase<T> {
   readonly $id?: I;
   readonly $item?: Item<T>;
-  readonly $entity: Entity<T>;
-
-  readonly isLoading: boolean;
-  readonly data?: T;
 }
 
 // Item builder
@@ -28,8 +45,14 @@ export function $item<T, I extends AegisId>(entity: Entity<T>, item: Query<Item<
 
 export function $item<T, I extends AegisId>(entity: Entity<T>, arg1: I | Query<Item<T>>, refresh?: () => Query<T>) {
   if (arg1 instanceof Query) {
+    const events = new EventSource<StoreEventMap<T> & QueryManagerEventMap<T>>();
+
     const item: AegisUnknownItem<T, I> = {
       $entity: entity,
+
+      get subscribe() {
+        return events.subscribe.bind(events);
+      },
 
       get isLoading() {
         return this.$item?.isLoading ?? (arg1.status === 'pending');
@@ -39,11 +62,26 @@ export function $item<T, I extends AegisId>(entity: Entity<T>, arg1: I | Query<I
       }
     };
 
-    arg1.subscribe('update.completed', (result) => {
+    // Events
+    arg1.subscribe('update.failed', (state, mtd) => {
+      events.emit('query.failed', state, { source: mtd.source });
+    });
+
+    arg1.subscribe('update.completed', ({ result }, mtd) => {
       Object.assign(item, {
-        $id: JSON.parse(result.result.id),
-        $item: result.result
+        $id: JSON.parse(result.id),
+        $item: result
       });
+
+      result.subscribe('update', (data, mtd) => {
+        events.emit(mtd.type, data, { source: mtd.source });
+      });
+
+      result.subscribe('query', (data, mtd) => {
+        events.emit(mtd.type, data, { source: mtd.source });
+      });
+
+      events.emit('query.completed', { status: 'completed', result: result.data! }, { source: mtd.source });
     });
 
     return item;
@@ -52,6 +90,10 @@ export function $item<T, I extends AegisId>(entity: Entity<T>, arg1: I | Query<I
       $id: arg1,
       $item: entity.item(JSON.stringify(arg1)),
       $entity: entity,
+
+      get subscribe() {
+        return this.$item.subscribe.bind(this.$item);
+      },
 
       get isLoading() {
         return this.$item.isLoading;
