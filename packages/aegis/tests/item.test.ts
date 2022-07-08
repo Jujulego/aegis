fe72b0c71c0a5e0f95ad078738375cc5f2c36df3
Julien Capellari <julien.capellari@gmail.com>
2022-07-08 14:15:50 +0200

    feat(aegis): test $item (known case)

--- packages/aegis/tests/item.test.ts
+++ packages/aegis/tests/item.test.ts
@@ -234,4 +234,131 @@ describe('$item', () => {
       expect(item.isLoading).toBe(false);
     });
   });
+
+  describe('Known item (id)', () => {
+    // Tests
+    it('should return data from store', () => {
+      const data = { id: 'test', success: true };
+      store.set(entity.name, JSON.stringify('test'), data);
+
+      const item = $item(entity, 'test');
+      expect(item.data).toBe(data);
+    });
+
+    it('should update stored data', () => {
+      const item = $item(entity, 'test');
+
+      // Update data
+      jest.spyOn(store, 'set');
+
+      const data = { id: 'toto', success: false };
+      item.data = data;
+
+      expect(store.set).toHaveBeenCalledWith(entity.name, item.$item?.id, data);
+      expect(item.data).toBe(data);
+    });
+
+    it('should emit store events (update)', () => {
+      const item = $item(entity, 'test');
+
+      // Register listener
+      const spy = jest.fn();
+      item.subscribe('update', spy);
+
+      // Emit event
+      store.set(entity.name, JSON.stringify('test'), { id: 'test', success: false });
+
+      expect(spy).toHaveBeenCalledWith(
+        {
+          id: JSON.stringify('test'),
+          new: { id: 'test', success: false }
+        },
+        {
+          type: `update.${entity.name}.${JSON.stringify('test')}`,
+          source: store,
+        }
+      );
+
+      expect(item.data).toEqual({ id: 'test', success: false });
+    });
+
+    it('should emit query manager events (query.pending)', () => {
+      const item = $item(entity, 'test');
+
+      // Register listener
+      const spy = jest.fn();
+      item.subscribe('query.pending', spy);
+
+      // Emit event
+      item.$item?.refresh(() => new Query(), 'replace');
+
+      expect(spy).toHaveBeenCalledWith(
+        {
+          status: 'pending'
+        },
+        {
+          type: 'query.pending',
+          source: item.$item?.manager,
+        }
+      );
+
+      expect(item.isLoading).toBe(true);
+    });
+
+    it('should emit query manager events (query.completed)', () => {
+      const item = $item(entity, 'test');
+
+      // Refresh query
+      const query = new Query<Test>();
+      item.$item?.refresh(() => query, 'replace');
+
+      // Register listener
+      const spy = jest.fn();
+      item.subscribe('query.completed', spy);
+
+      // Emit event
+      query.complete({ id: 'test', success: false });
+
+      expect(spy).toHaveBeenCalledWith(
+        {
+          status: 'completed',
+          result: { id: 'test', success: false }
+        },
+        {
+          type: 'query.completed',
+          source: query,
+        }
+      );
+
+      expect(item.isLoading).toBe(false);
+    });
+
+    it('should emit query manager events (query.failed)', () => {
+      const item = $item(entity, 'test');
+
+      // Refresh query
+      const query = new Query<Test>();
+      item.$item?.refresh(() => query, 'replace');
+
+      // Register listener
+      const spy = jest.fn();
+      item.subscribe('query.failed', spy);
+
+      // Emit event
+      query.fail(new Error('Failed !'));
+
+      expect(spy).toHaveBeenCalledWith(
+        {
+          status: 'failed',
+          error: new Error('Failed !')
+        },
+        {
+          type: 'query.failed',
+          source: query,
+        }
+      );
+
+      expect(item.isLoading).toBe(false);
+    });
+  });
 });
