0c9d9f20eb486fe888d6219f5cd1414f20bcf8a8
Julien Capellari <julien.capellari@gmail.com>
2022-07-09 16:52:43 +0200

    feat(react): adapt list hooks

--- packages/react/src/hook.ts
+++ packages/react/src/hook.ts
@@ -1,24 +1,35 @@
-import { AegisEntity, AegisId, AegisProtocol, AegisUnknownItem, Fetcher } from '@jujulego/aegis';
+import { AegisEntity, AegisId, AegisList, AegisProtocol, AegisUnknownItem, Fetcher } from '@jujulego/aegis';
 import { useMemo } from 'react';
-import { useSyncExternalStore } from 'use-sync-external-store/shim';
+
+import { useAegisItem, useAegisList, useDeepMemo } from './hooks';
 
 // Types
 export type AegisItemKeys<P extends AegisProtocol> = {
   [K in keyof P]: P[K] extends Fetcher<unknown[], AegisUnknownItem<unknown>> ? K : never;
 }[keyof P];
 
+export type AegisListKeys<P extends AegisProtocol> = {
+  [K in keyof P]: P[K] extends Fetcher<unknown[], AegisList<unknown>> ? K : never;
+}[keyof P];
+
 // Builder
 export function $hook<T, I extends AegisId, P extends AegisProtocol>(entity: AegisEntity<T, I, P>) {
   return {
     item<N extends AegisItemKeys<P>>(name: N) {
-      return function useAegisItem(...args: Parameters<P[N]>): ReturnType<P[N]> {
-        const item = useMemo(() => entity[name](...args) as AegisUnknownItem<T>, args);
+      return function useItem(...args: Parameters<P[N]>): ReturnType<P[N]> {
+        const _args = useDeepMemo(args);
+        const item = useMemo(() => entity[name](..._args) as AegisUnknownItem<T>, [_args]);
 
-        useSyncExternalStore((cb) => item.subscribe('query', cb), () => item.isLoading);
-        useSyncExternalStore((cb) => item.subscribe('update', cb), () => item.data);
-
-        return item as ReturnType<P[N]>;
+        return useAegisItem(item) as ReturnType<P[N]>;
       };
-    }
+    },
+    list<N extends AegisListKeys<P>>(name: N) {
+      return function useItem(...args: Parameters<P[N]>): ReturnType<P[N]> {
+        const _args = useDeepMemo(args);
+        const list = useMemo(() => entity[name](..._args) as AegisList<T>, [_args]);
+
+        return useAegisList(list) as ReturnType<P[N]>;
+      };
+    },
   };
 }
