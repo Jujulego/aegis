cf90ff702fc40b1f041d975e347431323c0feaad
Julien Capellari <julien.capellari@neoxia.com>
2022-08-05 17:35:14 +0200

    feat(react): adapt to event change

--- packages/react/src/hooks/useAegisItem.ts
+++ packages/react/src/hooks/useAegisItem.ts
@@ -4,7 +4,7 @@ import { useSyncExternalStore } from 'use-sync-external-store/shim';
 // Hooks
 export function useAegisItem<I extends AegisUnknownItem<any>>(item: I): I {
   // Register for updates
-  useSyncExternalStore((cb) => item.subscribe('query', cb), () => item.isLoading);
+  useSyncExternalStore((cb) => item.subscribe('status', cb), () => item.isLoading);
   useSyncExternalStore((cb) => item.subscribe('update', cb), () => item.data);
 
   return item;
--- packages/react/src/hooks/useAegisList.ts
+++ packages/react/src/hooks/useAegisList.ts
@@ -4,7 +4,7 @@ import { useSyncExternalStore } from 'use-sync-external-store/shim';
 // Hooks
 export function useAegisList<L extends AegisList<any>>(list: L): L {
   // Register for updates
-  useSyncExternalStore((cb) => list.subscribe('query', cb), () => list.isLoading);
+  useSyncExternalStore((cb) => list.subscribe('status', cb), () => list.isLoading);
   useSyncExternalStore((cb) => list.subscribe('update', cb), () => list.data);
 
   return list;
