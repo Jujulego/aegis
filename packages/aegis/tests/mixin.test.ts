8ef04c220a2084e645b7c56406f89d13d35fc9a8
Julien Capellari <julien.capellari@neoxia.com>
2022-08-11 18:15:35 +0200

    feat(aegis): add $mixin tests

--- packages/aegis/tests/mixin.test.ts
+++ packages/aegis/tests/mixin.test.ts
@@ -1,4 +1,4 @@
-import { Entity, MemoryStore } from '@jujulego/aegis-core';
+import { Entity, MemoryStore, Query } from '@jujulego/aegis-core';
 
 import { $mixin } from '../src';
 
@@ -23,5 +23,23 @@ describe('$mixin.entity', () => {
     expect(TestEntity.$entity).toBeInstanceOf(Entity);
     expect(TestEntity.$entity.name).toBe('Test');
     expect(TestEntity.$entity.store).toBe(store);
+  });
+
+  it('should add a static $item method returning a known item wrapper', () => {
+    class TestEntity extends $mixin.entity('Test', store, (test: Test) => test.id) {}
+
+    const item = TestEntity.$item('test');
+
+    expect(item.$id).toBe('test');
+    expect(item.$entity).toBe(TestEntity.$entity);
+  });
+
+  it('should add a static $list method returning a list wrapper', () => {
+    class TestEntity extends $mixin.entity('Test', store, (test: Test) => test.id) {}
+
+    const list = TestEntity.$list('test');
+
+    expect(list.$key).toBe('test');
+    expect(list.$entity).toBe(TestEntity.$entity);
   });
 });
