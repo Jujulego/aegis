e350cc36219bd86a70367fec016916aac3186187
Julien Capellari <julien.capellari@gmail.com>
2023-04-06 19:42:16 +0200

    feat: complete QRef

--- packages/aegis/src/queries/q-ref.ts
+++ packages/aegis/src/queries/q-ref.ts
@@ -1,12 +1,18 @@
-import { group, OffGroup, offGroup, once, source, waitFor } from '@jujulego/event-tree';
-import { Query, QueryStateDone, QueryStateFailed, QueryStatePending } from '@jujulego/utils';
+import { group, IListenable, IObservable, OffGroup, offGroup, once, source, waitFor } from '@jujulego/event-tree';
+import { Query, QueryState, QueryStateDone, QueryStateFailed, QueryStatePending } from '@jujulego/utils';
 
 // Types
 export type Fetcher<D> = () => Query<D>;
 export type Strategy = 'keep' | 'replace';
 
+export type QRefEventMap<D> = {
+  pending: QueryStatePending;
+  done: QueryStateDone<D>;
+  failed: QueryStateFailed;
+};
+
 // Class
-export class QRef<D> {
+export class QRef<D> implements IObservable<QueryState<D>>, IListenable<QRefEventMap<D>> {
   // Attributes
   private _off?: OffGroup;
   private _query?: Query<D>;
@@ -18,6 +24,11 @@ export class QRef<D> {
   });
 
   // Methods
+  readonly on = this._events.on;
+  readonly off = this._events.off;
+  readonly subscribe = this._events.subscribe;
+  readonly unsubscribe = this._events.unsubscribe;
+
   refresh(fetcher: Fetcher<D>, strategy: Strategy): Query<D> {
     // Keep previous query
     if (this._query?.status === 'pending' && strategy === 'keep') {
@@ -56,5 +67,13 @@ export class QRef<D> {
   // Properties
   get query(): Query<D> | undefined {
     return this._query;
+  }
+
+  get data(): D | undefined {
+    return this._query?.data;
+  }
+
+  get isLoading(): boolean {
+    return this._query?.status === 'pending';
   }
 }
