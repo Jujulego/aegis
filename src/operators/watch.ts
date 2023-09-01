67f6fa6a8d234799c974a1cec4d8b7033d09f260
Julien Capellari <julien.capellari@neoxia.com>
2023-09-01 18:28:50 +0200

    feat: add watch operator

---
+++ src/operators/watch.ts
@@ -0,0 +1,20 @@
+import { OffFn } from '@jujulego/event-tree';
+
+import { Ref } from '../refs/index.js';
+
+// Types
+export type WatchCleanUp = () => void;
+export type WatchFn<D> = (data: D) => WatchCleanUp | undefined;
+
+// Operator
+export function watch$<D>(ref: Ref<D>, fn: WatchFn<D>): OffFn {
+  let cleanUp: WatchCleanUp | undefined;
+
+  return ref.subscribe((data: D) => {
+    if (cleanUp) {
+      cleanUp();
+    }
+
+    cleanUp = fn(data);
+  });
+}
