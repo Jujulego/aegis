441cc7cea1727c0d31ead5536db26d2961d3c611
Julien Capellari <julien.capellari@gmail.com>
2023-09-05 16:49:14 +0200

    feat: export registry

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,6 @@ export * from './manager.js';
 export * from './operators/index.js';
 export * from './pipe.js';
 export * from './refs/index.js';
+export * from './registry.js';
 export * from './stores/index.js';
 export * from './watch.js';
