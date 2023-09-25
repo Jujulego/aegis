e5abf527d0162c831199a65c334e2ceb6804856a
Julien Capellari <julien.capellari@gmail.com>
2023-09-25 11:45:19 +0200

    feat: rework pipe tests

--- tests/pipe.test.ts
+++ tests/pipe.test.ts
@@ -1,15 +1,29 @@
+import { Observable, source$ } from '@jujulego/event-tree';
 import { vi } from 'vitest';
 
-import { const$, pipe$ } from '@/src/index.js';
+import { PipeContext } from '@/src/defs/pipe.js';
+import { pipe$ } from '@/src/pipe.js';
 
 describe('pipe$', () => {
   it('should use op to prepare result', () => {
-    const ref = const$('life');
-    const op = vi.fn(() => const$(42));
+    const ref = source$<string>();
+    const op = vi.fn((base: Observable<string>, { off }: PipeContext) => {
+      const res = source$<number>();
+      off.add(base.subscribe((v) => res.next(parseInt(v))));
 
+      return res;
+    });
+    const spy = vi.fn();
+
+    // Setup pipe
     const result = pipe$(ref, op);
 
-    expect(result.read()).toBe(42);
     expect(op).toHaveBeenCalledWith(ref, { off: result.off });
+
+    // Emit some data
+    result.subscribe(spy);
+    ref.next('42');
+
+    expect(spy).toHaveBeenCalledWith(42);
   });
 });
