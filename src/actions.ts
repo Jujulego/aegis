37cdc1c03bbcc3a2e8863af9782b33956cc1cd02
Julien Capellari <julien.capellari@gmail.com>
2023-11-12 00:29:07 +0100

    feat: document actions$

--- src/actions.ts
+++ src/actions.ts
@@ -18,6 +18,29 @@ export type ActionsRef<R extends SymmetricRef, A extends Record<string, ActionRe
   [K in keyof A]: A[K] extends ActionReducer<infer P, ObservedValue<R>> ? Action<P, R> : never;
 };
 
+/**
+ * Wrap a reference, adding some methods to modify stored data.
+ *
+ * Based on [immer](https://immerjs.github.io/immer/), those methods should return a reducer receiving current stored data.
+ * Then they can modify the data in place. Immer will produce an updated data objet that will be given to ref's mutate method
+ * to store the changes.
+ *
+ * @param ref reference to wrap (must be a symmetric reference)
+ * @param actions actions reducers
+ *
+ * @example
+ * const counter = action$(var$({ count: 1 }), {
+ *   add: (i: number) => (old) => {
+ *     old.count += i;
+ *   },
+ *   reset: () => (old) => {
+ *     old.count = 0;
+ *   },
+ * });
+ *
+ * counter.add(1); // <= this will increment count by 1
+ * counter.reset(); // <= this will reset count to 0
+ */
 export function actions$<R extends SymmetricRef, A extends ActionReducers<ObservedValue<R>>>(ref: R, actions: A): ActionsRef<R, A> {
   for (const [key, act] of Object.entries(actions)) {
     Object.assign(ref, {
