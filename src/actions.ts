feb1d029ea7e189cff9ce45405e3aee468cc215c
Julien Capellari <julien.capellari@gmail.com>
2023-11-12 11:59:37 +0100

    feat: move and document types

--- src/actions.ts
+++ src/actions.ts
@@ -1,22 +1,8 @@
+import { ObservedValue } from '@jujulego/event-tree';
 import { produce } from 'immer';
 
-import { AsyncMutable, AsyncReadable, MutableRef } from './defs/index.js';
+import { ActionReducers, ActionRef, SymmetricRef } from './defs/index.js';
 import { awaitedCall } from './utils/promise.js';
-import { ObservedValue } from '@jujulego/event-tree';
-
-// Types
-export type SymmetricRef<D = unknown> = MutableRef<D, D>;
-
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export type ActionReducers<D = unknown> = Record<string, ActionReducer<any[], D>>;
-export type ActionReducer<P extends unknown[], D> = (...params: P) => (old: D) => void | D;
-
-export type Action<P extends unknown[], R extends SymmetricRef> = (...params: P) => ActionResult<R, ObservedValue<R>>;
-export type ActionResult<R extends SymmetricRef, D> = R extends AsyncReadable | AsyncMutable ? Promise<D> : D;
-
-export type ActionsRef<R extends SymmetricRef, A extends Record<string, ActionReducer<unknown[], ObservedValue<R>>>> = R & {
-  [K in keyof A]: A[K] extends ActionReducer<infer P, ObservedValue<R>> ? Action<P, R> : never;
-};
 
 /**
  * Wrap a reference, adding some methods to modify stored data.
@@ -41,7 +27,7 @@ export type ActionsRef<R extends SymmetricRef, A extends Record<string, ActionRe
  * counter.add(1); // <= this will increment count by 1
  * counter.reset(); // <= this will reset count to 0
  */
-export function actions$<R extends SymmetricRef, A extends ActionReducers<ObservedValue<R>>>(ref: R, actions: A): ActionsRef<R, A> {
+export function actions$<R extends SymmetricRef, A extends ActionReducers<ObservedValue<R>>>(ref: R, actions: A): ActionRef<R, A> {
   for (const [key, act] of Object.entries(actions)) {
     Object.assign(ref, {
       [key]: (...params: unknown[]) => awaitedCall(
@@ -54,5 +40,5 @@ export function actions$<R extends SymmetricRef, A extends ActionReducers<Observ
     });
   }
 
-  return ref as ActionsRef<R, A>;
+  return ref as ActionRef<R, A>;
 }
